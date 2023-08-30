327409cafd7822cd04618ef347f7a1026ebdadde
Gary Woodfine <lnb0l9dc@duck.com>
2023-08-30 22:01:51 +0100

    Added fathom analytics

--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,18 @@
 
 
 import List from "~/components/app/list.vue";
+definePageMeta({ layout: 'default' })
 </script>
 
 <template>
-  <nuxt-layout name="default">
+
     <template #list >
      <list indexName="Sample_Phone_Store_Index" query="Samsung" />
     </template>
 
   <template #content> <p class="text-white">The Page Content will go here</p></template>
 
-  </nuxt-layout>
+
 
 </template>
 
