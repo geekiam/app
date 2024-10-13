0ce33afcaf66e2c004d76bc41acf298614b31909
Gary Woodfine <lnb0l9dc@duck.com>
2024-10-13 23:21:01 +0100

    Reformat module array in nuxt.config.ts
    Reordered the modules array for better readability and consistency. Ensured all settings related to Tailwind CSS, color mode, and image configurations remain unchanged.

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,37 +1,38 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-   modules: [
-     '@nuxtjs/tailwindcss',
-     '@nuxt/icon',
-     '@nuxtjs/color-mode',
-     '@pinia/nuxt',
-     '@nuxt/image',
-     '@vite-pwa/nuxt'
-   ],
-   tailwindcss: {
-       cssPath: '~/assets/css/tailwind.css',
-       configPath: 'tailwind.config',
-   },
-   colorMode: {
-       preference: 'light', // default value of $colorMode.preference
-       fallback: 'light', // fallback value if not system preference found
-       hid: 'nuxt-color-mode-script',
-       globalName: '__NUXT_COLOR_MODE__',
-       componentName: 'ColorScheme',
-       classPrefix: '',
-       classSuffix: '',
-       storageKey: 'nuxt-color-mode',
-   },
+    modules: [
+        '@pinia/nuxt',
+        '@vite-pwa/nuxt',
+        '@nuxtjs/tailwindcss',
+        '@nuxt/icon',
+        '@nuxtjs/color-mode',
+        '@nuxt/image'
+
+    ],
+    tailwindcss: {
+        cssPath: '~/assets/css/tailwind.css',
+        configPath: 'tailwind.config',
+    },
+    colorMode: {
+        preference: 'light', // default value of $colorMode.preference
+        fallback: 'light', // fallback value if not system preference found
+        hid: 'nuxt-color-mode-script',
+        globalName: '__NUXT_COLOR_MODE__',
+        componentName: 'ColorScheme',
+        classPrefix: '',
+        classSuffix: '',
+        storageKey: 'nuxt-color-mode',
+    },
     image: {
         provider: 'cloudinary',
         cloudinary: {
             baseURL: 'https://res.cloudinary.com/geekiam-io/image/upload/'
         }
     },
-   runtimeConfig: {
-       public: {
-           fathom_analytics_id: process.env.fathom_analytics_id,
-           siteUrl: 'https://geekiam.app',
-       },
-   }
+    runtimeConfig: {
+        public: {
+            fathom_analytics_id: process.env.fathom_analytics_id,
+            siteUrl: 'https://geekiam.app',
+        },
+    }
 })
