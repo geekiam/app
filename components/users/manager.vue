081aa6f5751029b85d3930901dd5d90a2574606c
Gary Woodfine <lnb0l9dc@duck.com>
2024-10-15 18:20:17 +0100

    Refactor auth handling and cleanup user manager
    Added routing check for authentication in `manager.vue` and adjusted image usage to improve compatibility. Cleaned up unnecessary comments and outdated code in `useAuthStore.ts`, improving maintainability.

--- components/users/manager.vue
+++ components/users/manager.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import {Menu, MenuButton, MenuItem, MenuItems} from "@headlessui/vue";
 import {useAuthStore} from "~/stores/useAuthStore";
+const router = useRouter();
 const auth = useAuthStore();
+
+if(!auth.isAuthenticated) {
+  await router.push("/")
+}
+
+
 
 const user = {
   name: auth.name,
   imageUrl: auth.image,
+  npub: auth.npub,
 
 }
+
 const userNavigation = [
   { name: 'Your Profile', href: 'user/profile' },
   { name: 'Sign out', href: '' },
@@ -19,7 +28,7 @@ const userNavigation = [
     <Menu as="div" class="relative inline-block text-left">
       <MenuButton class="flex rounded-full bg-white text-sm focus:outline-none focus:ring-2 focus:ring-green-600 focus:ring-offset-2">
         <span class="sr-only">Open user menu</span>
-        <nuxt-img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
+        <img class="h-8 w-8 rounded-full" :src="user.imageUrl" alt="" />
       </MenuButton>
 
       <transition enter-active-class="transition ease-out duration-100" enter-from-class="transform opacity-0 scale-95" enter-to-class="transform opacity-100 scale-100" leave-active-class="transition ease-in duration-75" leave-from-class="transform opacity-100 scale-100" leave-to-class="transform opacity-0 scale-95">
@@ -28,9 +37,6 @@ const userNavigation = [
             <MenuItem v-slot="{ active }" v-for="link in userNavigation" key="name">
               <a :href="link.href" :class="[active ? 'bg-orange-100' : '', 'block px-4 py-2 text-sm text-orange-700']">{{link.name}}</a>
             </MenuItem>
-<!--            <MenuItem v-slot="{ active }">
-              <a href="#" :class="[active ? 'bg-gray-100' : '', 'block px-4 py-2 text-sm text-gray-700']">Sign Out</a>
-            </MenuItem>-->
           </div>
         </MenuItems>
       </transition>
--- stores/useAuthStore.ts
+++ stores/useAuthStore.ts
@@ -1,18 +1,8 @@
 import {defineStore} from "pinia";
-import NDK, {NDKEvent, NDKNip07Signer, NDKUser} from "@nostr-dev-kit/ndk";
-import router from "#app/plugins/router";
-// Create a new NDK instance with explicit relays
-
-
-interface User {
-    id: string;
-}
-const geekiamKey = "geekiam-key";
-
 export const useAuthStore = defineStore('useAuthStore', {
     id: 'user-store',
     state: () => ({
-       npub:'',
+        npub:'',
         pubKey: '',
         name: '',
         image: '',
@@ -22,8 +12,6 @@ export const useAuthStore = defineStore('useAuthStore', {
     getters: {
         isAuthenticated: (state) => state.signedIn === true,
     },
-
-
     actions: {
         async profile(user,ndk){
                 user = ndk.getUser({
@@ -33,22 +21,15 @@ export const useAuthStore = defineStore('useAuthStore', {
                 await user.fetchProfile()
                 this.name = user.profile.name
                 this.image = user.profile.image
+                this.npub = user.npub
             }
         },
-      async  signIn(user,ndk) {
+      async signIn(user,ndk) {
 
           this.pubKey = user.pubKey
           await this.profile(user,ndk)
           this.signedIn = true
           this.signedOut = false
-        },
-        setUser(user: NDKUser) {
-            this.user = {id: user.npub}
-            localStorage.setItem(geekiamKey, JSON.stringify(this.user));
-        },
-        removeUser(){
-            this.user = null;
-            localStorage.removeItem(geekiamKey)
         },
        async signOut(){
           this.removeUser();
