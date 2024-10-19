bf73a4fc901ae91c65cccca7521724c72cdd6806
Gary Woodfine <lnb0l9dc@duck.com>
2024-10-19 16:13:52 +0100

    Add authentication page template
    Introduce a new authentication page at `pages/auth/index.vue` with a basic layout including left and right sections. Left section displays the brand icon and right section contains a welcoming message and a `nostr-signin` component.

---
+++ pages/auth/index.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <nuxt-layout name="auth">
+
+    <template #left>
+      <nuxt-img src="brand/icon" class="h-72 w-72" />
+    </template>
+    <template #right >
+      <div>
+        <p class="text-4xl font-bold text-white">What are you going to learn today? </p>
+        <nostr-signin />
+      </div>
+
+    </template>
+  </nuxt-layout>
+</template>
+
+<style scoped></style>
