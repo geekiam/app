32c375a2a2e1d2ba8dcc4259e1ac8cc886bd57df
Gary Woodfine <lnb0l9dc@duck.com>
2024-10-23 01:11:01 +0100

    Refactor sidebar layout for responsive design
    Updated the sidebar layout in `default.vue` to better handle responsiveness by switching to a `flex-row` structure for larger screens and simplifying the flex properties for both the `aside` and `section` elements. These changes improve the readability and maintainability of the code.

--- layouts/default.vue
+++ layouts/default.vue
@@ -3,24 +3,23 @@
     <geekiam-header />
     <div class="flex  min-h-screen flex-1 overflow-hidden">
       <side-bar />
-      <main class="min-w-0 flex-1 lg:flex ">
-        <aside class="lg:order-first ">
-          <div class="relative flex w-96 flex-col overflow-y-auto dark:bg-gray-800 border border-solid border-gray-700 p-2
-          dark:text-gray-200 text-gray-900">
+      <main class="flex flex-col lg:flex-row min-w-0 flex-1">
+        <aside class="flex-1 dark:bg-gray-800 border border-solid border-gray-700 p-2 dark:text-gray-200 text-gray-900">
+          <div class="relative flex flex-col overflow-y-auto">
             <slot name="list"/>
           </div>
         </aside>
-        <section aria-labelledby="primary-heading" class="hidden lg:flex-shrink-0 lg:block h-full min-w-0 flex-1 flex-col overflow-y-auto
-        lg:order-last dark:bg-gray-800 bg-gray-100 dark:text-gray-100">
-             <slot name="content" />
+        <section aria-labelledby="primary-heading" class="flex-1 h-full min-w-0 flex-col overflow-y-auto dark:bg-gray-800 bg-gray-100 dark:text-gray-100">
+          <slot name="content" />
         </section>
       </main>
     </div>
     <geekiam-footer/>
   </div>
 </template>
-#'
+<style lang="scss" scoped>
 
+</style>
 <script setup>
 
 
