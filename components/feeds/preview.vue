4a80aa3e50c01650431b7ec4712f50cddf26361c
Gary Woodfine <lnb0l9dc@duck.com>
2025-01-13 10:46:24 +0000

    Refactor feed preview component for cleaner code structure
    Removed redundant HTML and streamlined template structure to improve readability. Updated type annotation for articles to enhance type safety and adjusted formatting for consistency.

--- components/feeds/preview.vue
+++ components/feeds/preview.vue
@@ -1,69 +1,40 @@
 <script setup lang="ts">
 import {marked} from "marked";
 import {useArticlesStore} from '~/stores/articles';
-
+import type {Article} from "~/types";
 
 const {emit} = useMitter()
 const articlesStore = useArticlesStore();
 
 onMounted(async () => {
   await articlesStore.getArticles();
-
 })
 
 function select(id: string) {
   emit('selectedArticle', id);
 }
-const articles = articlesStore.articles;
+const articles = articlesStore.articles as Set<Article>;
 </script>
-
-
 <template>
   <section class="container mx-auto px-1 max-h-screen overflow-visible">
     <article v-for="article in articles" :key="article.id" class="border-2 border-gray-700 rounded-lg m-1 shadow-md">
-      <div class=" article-container content"   @click="select(article.id)">
+      <div class=" article-container content" @click="select(article.id)">
         <img v-if="article.image" :src="article.image" :alt="article.title" :title="article.title" class="image">
-        <nuxt-img v-else :alt="article.title" src="brand/question-mark" :title="article.title" loading="lazy" class="image"/>
+        <nuxt-img v-else :alt="article.title" src="brand/question-mark" :title="article.title" loading="lazy"
+                  class="image"/>
         <div class="flex flex-col flex-grow lg:max-w-full">
           <div class="flex items-center mb-2">
-            <feeds-author :author="article.author" lazy="true" class="mr-2" />
+            <feeds-author :author="article.author" lazy="true" class="mr-2"/>
             <p class="text-xs text-gray-400 ml-2">{{ article.published }}</p>
           </div>
-          <p class="text-lg font-semibold text-orange-500 justify-center m-2">{{article.title}}</p>
+          <p class="text-lg font-semibold text-orange-500 justify-center m-2">{{ article.title }}</p>
           <div v-if="article.summary" class="break-all ">
             <div v-html="marked.parse(article.summary)"></div>
           </div>
           <div class="flex justify-center gap-2 flex-wrap p-4">
-            <span class="text-md text-orange-300 ml-1" v-for="tag in article.tags" :key="tag">#{{tag}}</span>
+            <span class="text-md text-orange-300 ml-1" v-for="tag in article.tags" :key="tag">#{{ tag }}</span>
           </div>
         </div>
-<!--        <div
-            class="group image-container">
-          <img v-if="article.image" :src="article.image" :alt="article.title" :title="article.title" loading="lazy"
-               class="image"/>
-          <nuxt-img v-else :alt="article.title" src="brand/question-mark" loading="lazy" class="image"/>
-        </div>
-        &lt;!&ndash; Content Section &ndash;&gt;
-
-        <div class="flex flex-col flex-grow lg:max-w-full">
-          <div class="flex items-center gap-x-2 text-xs">
-            <feeds-author :author="article.author" :published="article.published" lazy="true" />
-          </div>
-          <p class="text-lg font-bold text-orange-500" v-if="article.title">
-            {{ article.title }}
-          </p>
-
-          <div v-if="article.summary" class="break-all ">
-            <div v-html="marked.parse(article.summary)"></div>
-          </div>
-      </div>
-
-
-
-      </div>
-      <div class="flex justify-center gap-2 flex-wrap p-4">
-        <span class="text-md text-orange-300 ml-1" v-for="tag in article.tags" :key="tag">#{{tag}}</span>
-      </div>-->
       </div>
     </article>
   </section>
@@ -74,15 +45,9 @@ const articles = articlesStore.articles;
   @apply grid gap-8 sm:grid-cols-2 sm:gap-12 lg:grid-cols-4 xl:grid-cols-2 xl:gap-16 cursor-pointer ;
   @apply flex flex-col items-center gap-4 md:flex-row lg:gap-9   lg:items-start lg:p-4 ;
 }
-
 .image {
   @apply w-28 h-28 object-cover rounded-t-lg mr-4 ml-2;
 }
-
-.image-container {
-  @apply relative block h-64 w-full shrink-0 self-start overflow-hidden rounded-lg shadow-lg md:h-24 md:w-24 lg:h-32 lg:w-32;
-}
-
 .content {
   @apply prose prose-sm max-w-full dark:text-gray-200 text-gray-800 sm:px-1
   dark:prose-blockquote:text-orange-500 prose-blockquote:text-xl
