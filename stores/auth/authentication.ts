0587ddf2697f0f46bd6907c58306f277695334d8
Gary Woodfine <lnb0l9dc@duck.com>
2025-01-14 16:23:30 +0000

    Enhance key handling in getPubKey with validation logic
    Added support for processing and validating `nsec` private keys. Introduced utility functions for hex-to-Uint8Array conversion and improved error handling for invalid keys. These changes ensure more robust and secure key operations.

--- stores/auth/authentication.ts
+++ stores/auth/authentication.ts
@@ -1,20 +1,44 @@
 
 import { bech32 } from "bech32";
-import { bytesToHex } from "@noble/hashes/utils";
-
-export async function getPubKey(key: string) : Promise<string> {
+import { bytesToHex,  } from "@noble/hashes/utils";
+import {getPublicKey, } from "nostr-tools";
+import * as secp from "@noble/secp256k1";
+export async function getPubKey(key: string) : Promise<string>  {
 
     if(key.startsWith("npub")) {
         return getHex(key)
     }
+    
+    if(key.startsWith("nsec")) {
+        let privateKey = getHex(key)
+        if(secp.utils.isValidPrivateKey(privateKey)) {
+            return getPublicKey(hexToUint8Array(privateKey))
+        }
+      }
 
-    return ""
+    if(secp.utils.isValidPrivateKey(key)) {
+        return getPublicKey(hexToUint8Array(getHex(key)))
+    }
+    throw new Error("Invalid key provided")
 }
 
 function getHex(key: string): string {
     return bytesToHex(
         Uint8Array.from(bech32.fromWords(bech32.decode(key).words))
     );
+}
+
+function hexToUint8Array(hex: string): Uint8Array {
+    if (hex.length % 2 !== 0) {
+        throw new Error("Invalid hex string");
+    }
+
+    const array = new Uint8Array(hex.length / 2);
+    for (let i = 0; i < hex.length; i += 2) {
+        array[i / 2] = parseInt(hex.substring(i, i + 2), 16);
+    }
+
+    return array;
 }
 
 
