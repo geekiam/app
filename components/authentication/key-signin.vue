564ad3cee0b3430fbfd21522653ff20ec7672643
Gary Woodfine <lnb0l9dc@duck.com>
2025-01-14 11:41:38 +0000

    Enhance UI styling and text consistency.
    Updated text colors and styles to improve visual clarity and alignment with the design theme. Adjusted input padding and icon size for improved usability and better appearance in the key-signin component. Fixed minor spacing and formatting inconsistencies across elements.

--- components/authentication/key-signin.vue
+++ components/authentication/key-signin.vue
@@ -6,7 +6,7 @@
   <div class="sign-up-container">
     <div class="relative mb-6">
       <div class="absolute inset-y-0 start-0 flex items-center ps-3.5 pointer-events-none">
-       <Icon name="material-symbols-light:key-outline" class="w-6 h-6 text-gray-400 mr-3" />
+       <Icon name="material-symbols-light:key-outline" class="w-8 h-8 mr-3 text-orange-500" />
       </div>
       <input type="text" id="input-group-1" class="input-style" placeholder="npub / nsec / hex">
     </div>
@@ -23,7 +23,7 @@
 
 <style scoped>
 .input-style {
-  @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-orange-500 focus:border-orange-500 block w-full ps-10 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500;
+  @apply bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-3xl focus:ring-orange-500 focus:border-orange-500 block w-full ps-12 p-2.5  dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-orange-500 dark:focus:border-orange-500;
 }
 
 .button-style {
--- pages/auth/index.vue
+++ pages/auth/index.vue
@@ -5,42 +5,44 @@
 <template>
   <nuxt-layout name="auth">
     <template #left>
-      <nuxt-img src="brand/icon" class="h-72 w-72" />
+      <nuxt-img src="brand/icon" class="h-72 w-72"/>
     </template>
-    <template #right >
+    <template #right>
       <div>
-        <p class="text-4xl font-bold text-style">What are you going to learn today? </p>
-        <p class="text-style mb-10">Geekiam is built using the <a href="https://nostr.com/" class="underline" target="_blank">nostr protocol</a> which enables you to own
-        and manage your social identity</p>
+        <p class="text-4xl font-bold text-orange-500">What are you going to learn today? </p>
+        <p class="text-style mb-10">Geekiam is built using the <a href="https://nostr.com/" class="underline"
+                                                                  target="_blank">nostr protocol</a> which enables you
+          to own
+          and manage your social identity</p>
 
         <div>
-          <authentication-key-signin />
+          <authentication-key-signin/>
         </div>
-
         <div class="container">
-          <hr class="line" />
-          <span class="or">or</span>
-          <hr class="line" />
+          <hr class="line"/>
+          <span class="or">Or</span>
+          <hr class="line"/>
         </div>
-<div>
-  <authentication-extension-signer />
-</div>
+        <div>
+          <authentication-extension-signer/>
+        </div>
       </div>
     </template>
   </nuxt-layout>
 </template>
 
 <style scoped>
-.text-style{
+.text-style {
   @apply dark:text-gray-100 text-gray-900 mb-10 mt-10;
 }
+
 .container {
-  @apply  flex text-center justify-center items-center mb-10;
+  @apply flex text-center justify-center items-center mb-10;
 
 }
 
 .line {
-   @apply flex-grow m-2;
+  @apply flex-grow m-2;
 }
 
 .or {
