ef5b1d9e21c5c21a225f52e4b80b04a592c549cb
Gary Woodfine <lnb0l9dc@duck.com>
2025-01-14 15:19:00 +0000

    Fix import formatting and update dependency version range
    Corrected spacing in an import in `authentication.ts` for clarity. Updated the `util` package version in `package.json` to use a caret (^) for more flexible versioning.

--- stores/auth/authentication.ts
+++ stores/auth/authentication.ts
@@ -1,13 +1,12 @@
 
 import { bech32 } from "bech32";
-import  {bytesToHex} from "@noble/hashes/utils";
+import { bytesToHex } from "@noble/hashes/utils";
 export async function getPubKey(key: string) : Promise<string> {
 
 
 
     if(key.startsWith("npub")) {
-        let hex = getHex(key);
-        return hex
+        return  getHex(key)
     }
 
     return ""
