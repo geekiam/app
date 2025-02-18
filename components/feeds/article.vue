6617e85bf6027e153cdd71bf8f885ac5645c4cc7
Gary Woodfine <lnb0l9dc@duck.com>
2025-02-18 14:53:17 +0000

    Add invert styling for inline code elements in articles
    This change applies an `invert` styling to `prose-code` elements, improving readability in both light and dark modes. Existing styles for other elements remain unchanged.

--- components/feeds/article.vue
+++ components/feeds/article.vue
@@ -54,7 +54,7 @@ const article = computed(() => articlesStore.selectedArticle) ;
   @apply prose prose-base text-base max-w-full dark:text-gray-200 text-gray-800 sm:px-1
   dark:prose-blockquote:text-orange-500 prose-blockquote:text-lg
   prose-headings:text-orange-500 prose-a:no-underline
-  prose-a:text-orange-500 dark:prose-a:text-orange-500  prose-strong:text-orange-500;
+  prose-a:text-orange-500 dark:prose-a:text-orange-500  prose-strong:text-orange-500 prose-code:invert;
 
 
 }
